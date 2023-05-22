8069d1fd2820111072489af12ebc3df446e6a965
Raduc2003 <72871085+Raduc2003@users.noreply.github.com>
2023-05-23 00:53:14 +0300

    y

--- main.cpp
+++ main.cpp
@@ -2,52 +2,82 @@
 #include <string>
 #include <vector>
 using namespace std;
+class Planner
+{
 
-class Planner{
-    
-    private:
-        Planner()=default;
+private:
+    Planner() = default;
 
-    public:
-        Planner(const Planner &obj) = delete;
-        static Planner &getPlanner() { 
-             
-            static Planner instance;
-            return  instance;
-
-        }
-
-};
-class DayPlan{
-    protected:
-        int Task;
-    public:
-        DayPlan(){
-
-            
-        }
-
-};
-class MonthPlan: public DayPlan{
-    protected:
-        DayPlan month[31];
-    public:
-        MonthPlan():DayPlan(){
-
-
-        };
-};
-class YearPlan: public MonthPlan{
-    protected:
-        MonthPlan month[12];
-    public:
-        YearPlan():MonthPlan(){};
+public:
+    Planner(const Planner &obj) = delete;
+    static Planner &getPlanner()
+    {
+        static Planner instance;
+        return instance;
+    }
 };
 
-int main(){
+template <typename T>
+class Task
+{
+private:
+    char title[30];
+    bool done;
 
-    Planner  &Planner1 = Planner::getPlanner();
+public:
+    Task()
+    {
+        this->title = "Untitled Task";
+        bool done = false;
+    }
+    Task(char title)
+    {
+        this->title = title;
+    }
+};
+class Task<string>
+{
+private:
+    string description;
 
+public:
+    Task(string description)
+    {
+        this->description = description;
+    }
+};
 
+class DayPlan
+{
+protected:
+    vector<Task<char>> tasks;
 
+public:
+    DayPlan()
+    {
+    }
+};
+class MonthPlan : public DayPlan
+{
+protected:
+    DayPlan month[31];
+
+public:
+    MonthPlan() : DayPlan(){
+
+                  };
+};
+class YearPlan : public MonthPlan
+{
+protected:
+    MonthPlan month[12];
+
+public:
+    YearPlan() : MonthPlan(){};
+};
+
+int main()
+{
+
+    Planner &Planner1 = Planner::getPlanner();
 }
